f53f0ac5fed5f50ee3b2f417ca9fff2d16163db2
maria <maria.calani.uvaldez@gmail.com>
2025-07-25 20:05:03 -0400

    Se agrego con la historia de usuario

--- gitlearning/MC_errorDepartamentos.py
+++ gitlearning/MC_errorDepartamentos.py
@@ -1,13 +1,20 @@
+import pytest
 import requests
 
-url = "https://collectionapi.metmuseum.org/public/collection/v1/habitaciones"
 
-payload = ""
-headers = {
-  'Cookie': 'incap_ses_1720_1662004=wcCyGGMsF2m1z4Y+QKveF4UQhGgAAAAAdYkCo4j0+7mYIvTmH2HyZQ==; visid_incap_1662004=Q/Qivtu9RgiQGNrtHytd6QckgGgAAAAAQUIPAAAAAAAATI5qPUOY0nqVVjYfylO8'
-}
+@pytest_mark_negativo 
 
-response = requests.request("GET", url, headers=headers, data=payload)
+def Test_No_Muestra_Los_departamentos():
+    
+    url = "https://collectionapi.metmuseum.org/public/collection/v1/habitaciones"
 
-print(response.text)
-assert response.status_code == 400
+    payload = ""
+    headers = {
+    'Cookie': 'incap_ses_1720_1662004=wcCyGGMsF2m1z4Y+QKveF4UQhGgAAAAAdYkCo4j0+7mYIvTmH2HyZQ==; visid_incap_1662004=Q/Qivtu9RgiQGNrtHytd6QckgGgAAAAAQUIPAAAAAAAATI5qPUOY0nqVVjYfylO8'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+    assert response.status_code == 400
+    #Prioridad: Media
