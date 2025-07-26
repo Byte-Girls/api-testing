2c37f70fba88a7083f978e8c5ee76348e8201edd
Jhesabel <201803483@est.umss.edu>
2025-07-25 20:16:33 -0400

    JCE_SegundaPractica

--- gitlearning/JCESegundaPractica.py
+++ gitlearning/JCESegundaPractica.py
@@ -1,10 +1,26 @@
+
+import pytest
 import requests
 
+@pytest.mark.smoke
+def test_Obtener_objeto_no_existente_():
+
+ # Ambiente
 url = "https://jsonplaceholder.typicode.com/posts/999999"
 
-payload = ""
-headers = {}
+# Pasos
+    # 1. Seleccionar GET
+list_url = url +"objects"
+  #2.llamar al recurso objects
+  #3. Click en el boton send
+  response = requests.get(list_url)
+  #4. verificar que el estado sea 404
 
-response = requests.request("GET", url, headers=headers, data=payload)
+assert response.status_code==404
 
-print(response.text)
+#Prioridad
+# media.
+#Post Condición
+#Teardown
+#Clasificación
+#Funcional, Smoke,Negative.
