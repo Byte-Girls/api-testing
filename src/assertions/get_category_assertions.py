5e6367d8826ce701432083c62a905b11cd39709d
Jhesabel <201803483@est.umss.edu>
2025-08-01 13:35:15 -0400

    BYT-16 :Se añadio validación de schema para el get

--- src/assertions/get_category_assertions.py
+++ src/assertions/get_category_assertions.py
@@ -2,11 +2,9 @@ from src.utils.load_resources import load_schema_resource
 import jsonschema
 import pytest
 
-def assert_create_category_response_schema(response):
-    schema = load_schema_resource("create_category_schema_response.json")
+def assert_get_category_response_schema(response):
+    schema = load_schema_resource("get_category_schema_response.json")
     try:
         jsonschema.validate(instance=response.json(), schema=schema)
     except jsonschema.exceptions.ValitoionError  as err:
         pytest.fail(f"JSON schema no coincide. Error: {err}")
-
-    
--- tests/opensource_demo_orangehrmlive/admin/job-categories/test_get_category.py
+++ tests/opensource_demo_orangehrmlive/admin/job-categories/test_get_category.py
@@ -1,12 +1,16 @@
 import requests
 import json
 import pytest
+from src.assertions.get_category_assertions import *
 
 @pytest.mark.smoke
 @pytest.mark.regression
 @pytest.mark.funcional
 def test_BYT_T2_Obtener_una_categoria_de_trabajo_existente_con_id_válido(get_url,get_token):
-#Descripción:  Verificar que el administrador pueda consultar una categoría de trabajo existente proporcionando un ID válido.
+  """
+  Descripción:  Verificar que el administrador pueda consultar una categoría de trabajo existente 
+  proporcionando un ID válido.
+  """
   url = f"{get_url}/admin/job-categories/1" 
 
   response =requests.get(url) 
@@ -14,5 +18,6 @@ def test_BYT_T2_Obtener_una_categoria_de_trabajo_existente_con_id_válido(get_ur
     'Authorization': f'{get_token}'
   }
   response = requests.get(url, headers=headers)
-  assert response.status_code ==200
+  assert response.status_code == 200
+  assert_get_category_response_schema(response)  
   
