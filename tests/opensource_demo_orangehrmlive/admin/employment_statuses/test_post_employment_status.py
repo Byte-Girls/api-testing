b1eb77748d58733507022097e9d68618144c9e21
maria <maria.calani.uvaldez@gmail.com>
2025-08-02 00:51:21 -0400

    BYT-35 crea un estado de empleado con campo (name) vacio

--- tests/opensource_demo_orangehrmlive/admin/employment_statuses/test_post_employment_status.py
+++ tests/opensource_demo_orangehrmlive/admin/employment_statuses/test_post_employment_status.py
@@ -1,43 +1,48 @@
-import random
-import requests
 import json
-import jsonschema
+import random
 import pytest
-from src.assertions.create_employment_status_assertions import *  
+import requests
+from src.assertions.common_assertions import * 
     
 @pytest.mark.funtional_positive
 @pytest.mark.regression
-def test_BYT_T37_crear_un_estado_de_empleado(get_url, get_token):
+def test_BYT_T37_crear_un_estado_de_empleado(statuses_url, header):
     """
     Descripción: El admin debe poder crear un nuevo estado de empleado
     """
     
-    url = f"{get_url}/admin/employment-statuses"
-    expected_name = "Maria" + str(random.randint(1000, 9999))
-    
     payload = json.dumps({
-        "name" : expected_name
+        "name" : "Maria" + str(random.randint(1000, 9999))
     })
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization":  f'{get_token}'
-    }
-
-    response = requests.post(url, headers=headers, data=payload)
+    
+    response = requests.post(statuses_url, headers=header, data=payload)
     assert response.status_code == 200
-    assert_create_employment_status_schema_response(response)
-    response=response.json()["data"]
-    assert response["name"] == expected_name
+    assert_resource_response_schema(response, "create_employment_status_schema_response.json")
+    response_data = response.json()["data"]
+    #comparación directa con el campo
+    expected_payload_dict = json.loads(payload)
+    assert response_data["name"] == expected_payload_dict["name"]
     
     #validación del get
-    url = f"{get_url}/admin/employment-statuses/{response['id']}"
-
-    headers = {
-        "Authorization": f'{get_token}'
-        }
-        
-    response = requests.get(url, headers=headers)
+    url = f"{statuses_url}/{response_data['id']}"
+    response = requests.get(url, headers=header)
     assert response.status_code == 200
-    response = response.json()["data"]
-    assert response["name"] == expected_name
+    response_data = response.json()["data"]
+    # comparacion dierecta campo a campo
+    assert response_data["name"] == expected_payload_dict["name"]
+
+
+@pytest.mark.valor_limite
+@pytest.mark.regression
+def test_BYT_T40_crear_estado_con_nombre_de_51_caracteres(statuses_url, header):
+    """
+    Descripción: El admin debe poder crear un nuevo estado de empleado
+    """
     
+    payload = json.dumps({
+        "name" : "Lorem Ipsum is simply dummy text of the printing an" 
+    })
+    
+    response = requests.post(statuses_url, headers=header, data=payload)
+    assert response.status_code == 422
+  
