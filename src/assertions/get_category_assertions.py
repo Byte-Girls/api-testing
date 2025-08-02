ad1fd1431e4c347a3329cf26434250178e777f0b
unknown <carolina.melendez376@gmail.com>
2025-08-01 21:56:08 -0400

    BYT-14: Implementación del test case obtener usuario con id valido mas refactorización en conftest para url y headers

--- src/assertions/get_category_assertions.py
+++ src/assertions/get_category_assertions.py
@@ -1,10 +1,2 @@
-from src.utils.load_resources import load_schema_resource
-import jsonschema
-import pytest
+# Añadir assertions especificos para el recurso de get category
 
-def assert_get_category_response_schema(response):
-    schema = load_schema_resource("get_category_schema_response.json")
-    try:
-        jsonschema.validate(instance=response.json(), schema=schema)
-    except jsonschema.exceptions.ValitoionError  as err:
-        pytest.fail(f"JSON schema no coincide. Error: {err}")
