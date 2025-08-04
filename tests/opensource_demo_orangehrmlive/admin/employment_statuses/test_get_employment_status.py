61b79c799ba4afadfb0061a6a7e8393a5d5c47b2
maria <maria.calani.uvaldez@gmail.com>
2025-08-03 21:04:29 -0400

    BYT-11 Get employment status, se agrego los test Bbyt-t80,byt-t81, byt-t82 y byt-t84

--- tests/opensource_demo_orangehrmlive/admin/employment_statuses/test_get_employment_status.py
+++ tests/opensource_demo_orangehrmlive/admin/employment_statuses/test_get_employment_status.py
@@ -2,9 +2,11 @@ import requests
 import pytest
 import string
 import random
+import json
 from src.assertions.common_assertions import *
 
 @pytest.mark.smoke
+@pytest.mark.positivo
 @pytest.mark.regression
 def test_BYT_T79_obtener_informacion_de_un_estado_de_empleado(statuses_url, header):
     """
@@ -15,7 +17,7 @@ def test_BYT_T79_obtener_informacion_de_un_estado_de_empleado(statuses_url, head
     assert response.status_code == 200
     assert_resource_response_schema(response, "get_employment_status_schema_response.json")  
 
-@pytest.mark.smoke
+@pytest.mark.negativo
 @pytest.mark.regression
 def test_BYT_T42_obtener_informacion_de_un_estado_con_ID_de_letras(statuses_url, header):
     """
@@ -26,7 +28,7 @@ def test_BYT_T42_obtener_informacion_de_un_estado_con_ID_de_letras(statuses_url,
     response = requests.get(url, headers=header)
     assert response.status_code == 422
 
-@pytest.mark.smoke
+@pytest.mark.negativo
 @pytest.mark.regression
 def test_BYT_T43_obtener_informacion_de_un_estado_con_token_invalido(statuses_url):
     """
@@ -42,16 +44,84 @@ def test_BYT_T43_obtener_informacion_de_un_estado_con_token_invalido(statuses_ur
     response = requests.get(url, headers=headers_invalidos)
     assert response.status_code == 401
 
-@pytest.mark.smoke
+@pytest.mark.negativo
 @pytest.mark.regression
 def test_BYT_T44_obtener_informacion_de_un_estado_con_caracteres_especiales(statuses_url, header):
     """
     Descripción: El admin intenta obtener un estado usando un ID que contiene solo caracteres especiales.
-    El sistema debe rechazar la solicitud con un error como 400 o 404.
+    El sistema debe rechazar la solicitud con un error 422.
     """
-    caracteres_especiales = ''.join(random.choices(string.punctuation, k=2))
-    url = f"{statuses_url}/{caracteres_especiales}"
+    url = f"{statuses_url}/@$"
     
     response = requests.get(url, headers=header)
     assert response.status_code == 422
+
+@pytest.mark.seguridad
+@pytest.mark.negativo
+@pytest.mark.regression
+def test_BYT_T46_obtener_informacion_del_estado_sin_token_de_autorizacion(statuses_url):
+    """
+    Descripción: El admin intenta obtener el estado, sin token de autorización
+    """
+    url = f"{statuses_url}/3"
+    headers_sin_token = {
+        "Content-Type": "application/json"
+        # Sin 'Authorization'
+    }
+    response = requests.get(url, headers=headers_sin_token)
+    assert response.status_code == 401
+
+@pytest.mark.negativo
+@pytest.mark.regression
+def test_BYT_T80_obtener_informacion_del_estado_con_id_0(statuses_url, header):
+    """
+    Descripción: El admin intenta obtener el estado, del id 0
+    """
+    url = f"{statuses_url}/0"
+    
+    response = requests.get(url, headers=header)
+    assert response.status_code == 422
+
+@pytest.mark.negativo
+@pytest.mark.regression
+def test_BYT_T81_obtener_informacion_del_estado_con_nombre_del_id(statuses_url, header):
+    """
+    Descripción: El admin crea un estado, y quiere obtener información del estado con el nombre del estado creado
+    """
+    
+    payload = json.dumps({
+        "name" : "Flash" + str(random.randint(1000, 9999))
+    })
+    nombre=payload
+    
+    response = requests.post(statuses_url, headers=header, data=payload)
+    assert response.status_code == 200
+    assert_resource_response_schema(response, "create_employment_status_schema_response.json")
+    
+    url = f"{statuses_url}/{nombre}"
+    response = requests.get(url, headers=header)
+    assert response.status_code == 422
+    
+@pytest.mark.negativo
+@pytest.mark.regression
+def test_BYT_T84_obtener_informacion_del_estado_con_ID_inexistente(statuses_url, header):
+    """
+    Descripción: El admin intenta obtener el estado, del id que no existe
+    """
+    id_inexistente = random.randint(10000, 99999)
+    url = f"{statuses_url}/{id_inexistente}"
+    
+    response = requests.get(url, headers=header)
+    assert response.status_code == 404
+
+@pytest.mark.negativo
+@pytest.mark.regression
+def test_BYT_T82_obtener_informacion_del_estado_sin_mandar_el_ID(statuses_url, header):
+    """
+    Descripción: El admin intenta obtener información del estado, sin mandar el id
+    """
+    url = f"{statuses_url}/"
+    
+    response = requests.get(url, headers=header)
+    assert response.status_code == 404  
     
