0ffcfb205b1ee4b30692acb9edaea6517ed8c21a
unknown <carolina.melendez376@gmail.com>
2025-08-12 23:15:54 -0400

    BYT-23: Se añadio clase especifica para la libreria requests

---
+++ src/orange_api/api_request.py
@@ -0,0 +1,20 @@
+import requests
+
+class OrangeRequest:
+    @staticmethod
+    def get(url, headers):
+        return requests.get(url, headers=headers)
+    
+    @staticmethod
+    def post(url, headers, payload):
+        return requests.post(url, headers=headers, data=payload)
+    
+    @staticmethod
+    def put(url, headers, payload):
+        return requests.post(url, headers=headers, data=payload)
+    
+    @staticmethod
+    def delete(url, headers, payload):
+        return requests.get(url, headers=headers, data=payload)
+    
+    
